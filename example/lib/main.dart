b94013aff2e423234f8cc2e92cd2e18d8621ab56
Jonas Bark <jonas.bark@ffuf.de>
2018-01-29 22:11:13 +0100

    update example, documentation

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:stripe_payment/stripe_source.dart';
+import 'package:stripe_payment/stripe_payment.dart';
 
 void main() {
   runApp(new MyApp());
@@ -17,27 +17,9 @@ class _MyAppState extends State<MyApp> {
   @override
   initState() {
     super.initState();
-    initPlatformState();
-  }
-
-  // Platform messages are asynchronous, so we initialize in an async method.
-  initPlatformState() async {
-    String platformVersion;
-    // Platform messages may fail, so we use a try/catch PlatformException.
-    try {
-      platformVersion = await StripeSource.platformVersion;
-    } on PlatformException {
-      platformVersion = 'Failed to get platform version.';
-    }
-
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
-    if (!mounted)
-      return;
-
-    setState(() {
-      _platformVersion = platformVersion;
+    StripeSource.setPublishableKey("pk_test");
+    StripeSource.addSource.then((String token) {
+      _addSource(token);
     });
   }
 
--- lib/stripe_payment.dart
+++ lib/stripe_payment.dart
@@ -6,10 +6,14 @@ class StripeSource {
 
   static const MethodChannel _channel = const MethodChannel('stripe_payment');
 
+  /// opens the stripe dialog to add a new card
+  /// if the source has been successfully added the card token will be returned
   static Future<String> get addSource => _channel.invokeMethod('addSource');
 
   static bool _publishableKeySet = false;
 
+  /// set the publishable key that stripe should use
+  /// call this once and before you use [addSource]
   static void setPublishableKey(String apiKey) {
     _channel.invokeMethod('setPublishableKey', apiKey);
     _publishableKeySet = true;
