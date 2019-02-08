ec23a6f3c7cd2ae328ced520040c4adbdf43829e
Rody Davis <rdavis@unifycrm.com>
2019-02-08 07:27:03 -0500

    AndroidX Support

--- example/android/app/src/main/kotlin/jonasbark/de/stripepaymentexample/MainActivity.kt
+++ example/android/app/src/main/kotlin/jonasbark/de/stripepaymentexample/MainActivity.kt
@@ -1,7 +1,7 @@
 package jonasbark.de.stripepaymentexample
 
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import de.jonasbark.stripepayment.StripeDialog
 
 class MainActivity : AppCompatActivity() {
