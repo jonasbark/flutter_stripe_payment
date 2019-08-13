8e7e64434f0eb7cd3857ae25d8ca36b31e75fb71
Jonas Bark <jonas.bark@ffuf.de>
2019-08-13 19:00:29 +0200

    android: add payment method using new Payment Intent instead of source API

--- android/src/main/java/de/jonasbark/stripepayment/StripeDialog.kt
+++ android/src/main/java/de/jonasbark/stripepayment/StripeDialog.kt
@@ -5,10 +5,11 @@ import com.google.android.material.snackbar.Snackbar
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.stripe.android.ApiResultCallback
+import com.stripe.android.PaymentConfiguration
 import com.stripe.android.SourceCallback
 import com.stripe.android.Stripe
-import com.stripe.android.model.Source
-import com.stripe.android.model.SourceParams
+import com.stripe.android.model.*
 import com.stripe.android.view.CardMultilineWidget
 import java.lang.Exception
 
@@ -57,7 +58,8 @@ class StripeDialog : androidx.fragment.app.DialogFragment() {
     var tokenListener: ((String) -> (Unit))? = null
 
     private fun getToken() {
-        val mCardInputWidget = view?.findViewById<View>(R.id.card_input_widget) as CardMultilineWidget
+        val mCardInputWidget =
+            view?.findViewById<View>(R.id.card_input_widget) as CardMultilineWidget
 
         if (mCardInputWidget.validateAllFields()) {
 
@@ -67,32 +69,45 @@ class StripeDialog : androidx.fragment.app.DialogFragment() {
                 view?.findViewById<View>(R.id.buttonSave)?.visibility = View.GONE
 
                 val publishableKey = arguments?.getString("publishableKey", null) ?: ""
+                PaymentConfiguration.init(publishableKey)
 
-                val stripe = Stripe(activity!!, publishableKey)
-                stripe.createSource(SourceParams.createCardParams(card), object : SourceCallback {
-                    override fun onSuccess(result: Source) {
-                        view?.findViewById<View>(R.id.progress)?.visibility = View.GONE
-                        view?.findViewById<View>(R.id.buttonSave)?.visibility = View.GONE
+                val paymentMethodParamsCard = card.toPaymentMethodParamsCard()
+                val paymentMethodCreateParams = PaymentMethodCreateParams.create(
+                    paymentMethodParamsCard,
+                    PaymentMethod.BillingDetails.Builder().build()
+                )
 
-                        if (result.id != null) {
-                            tokenListener?.invoke(result.id!!)
-                            dismiss()
+                val stripe = Stripe(activity!!, PaymentConfiguration.getInstance().publishableKey)
+
+                stripe.createPaymentMethod(
+                    paymentMethodCreateParams,
+                    object : ApiResultCallback<PaymentMethod> {
+                        override fun onSuccess(result: PaymentMethod) {
+                            view?.findViewById<View>(R.id.progress)?.visibility = View.GONE
+                            view?.findViewById<View>(R.id.buttonSave)?.visibility = View.GONE
+
+                            if (result.id != null) {
+                                tokenListener?.invoke(result.id!!)
+                                dismiss()
+                            }
                         }
-                    }
 
-                    override fun onError(error: Exception) {
-                        view?.findViewById<View>(R.id.progress)?.visibility = View.GONE
-                        view?.findViewById<View>(R.id.buttonSave)?.visibility = View.VISIBLE
-                        view?.let {
-                            Snackbar.make(it, error.localizedMessage, Snackbar.LENGTH_LONG).show()
+                        override fun onError(error: Exception) {
+                            view?.findViewById<View>(R.id.progress)?.visibility = View.GONE
+                            view?.findViewById<View>(R.id.buttonSave)?.visibility = View.VISIBLE
+                            view?.let {
+                                Snackbar.make(it, error.localizedMessage, Snackbar.LENGTH_LONG)
+                                    .show()
+                            }
                         }
-                    }
 
-                })
+                    })
+
             }
         } else {
             view?.let {
-                Snackbar.make(it, "The card info you entered is not correct", Snackbar.LENGTH_LONG).show()
+                Snackbar.make(it, "The card info you entered is not correct", Snackbar.LENGTH_LONG)
+                    .show()
             }
         }
 
