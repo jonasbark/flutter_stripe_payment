b6ada3b660dd31311614c8f6210a96a0f3c73711
Jocelyn Girard <jocelyn@geev.com>
2019-10-17 18:53:44 +0200

    Reverts the imports reordering.

--- lib/src/stripe_payment.dart
+++ lib/src/stripe_payment.dart
@@ -3,15 +3,15 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
-import 'android_pay_payment_request.dart';
-import 'apple_pay_payment_request.dart';
-import 'card_form_payment_request.dart';
-import 'error_codes.dart';
 import 'payment_intent.dart';
 import 'payment_method.dart';
 import 'source.dart';
 import 'source_params.dart';
 import 'token.dart';
+import 'android_pay_payment_request.dart';
+import 'apple_pay_payment_request.dart';
+import 'card_form_payment_request.dart';
+import 'error_codes.dart';
 
 class StripePayment {
   static const MethodChannel _channel = const MethodChannel('stripe_payment');
