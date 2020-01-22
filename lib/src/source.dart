1e5ef05a235926377d416aaea4fd40bdc5d4368f
eyoeldefare <edefare@masonlive.gmu.edu>
2020-01-22 13:39:48 -0500

    Fixed some warning messages that are suggested by dartanalyzer

--- lib/src/source.dart
+++ lib/src/source.dart
@@ -3,9 +3,9 @@ import 'package:stripe_payment/src/card_form_payment_request.dart';
 class Source {
   String object;
   Receiver receiver;
-  AchCreditTransfer ach_credit_transfer;
+  AchCreditTransfer achCreditTransfer;
   num amount;
-  String client_secret;
+  String clientSecret;
   num created;
   String currency;
   String flow;
@@ -13,7 +13,7 @@ class Source {
   bool livemode;
   Map<dynamic, dynamic> metadata;
   Owner owner;
-  String statement_descriptor;
+  String statementDescriptor;
   String status;
   String type;
   String usage;
@@ -21,9 +21,9 @@ class Source {
   Source(
       {this.object,
       this.receiver,
-      this.ach_credit_transfer,
+      this.achCreditTransfer,
       this.amount,
-      this.client_secret,
+      this.clientSecret,
       this.created,
       this.currency,
       this.flow,
@@ -31,7 +31,7 @@ class Source {
       this.livemode,
       this.metadata,
       this.owner,
-      this.statement_descriptor,
+      this.statementDescriptor,
       this.status,
       this.type,
       this.usage});
@@ -40,10 +40,10 @@ class Source {
     return Source(
       object: json['object'],
       receiver: json['receiver'] != null ? Receiver.fromJson(json['receiver']) : null,
-      ach_credit_transfer:
+      achCreditTransfer:
           json['ach_credit_transfer'] != null ? AchCreditTransfer.fromJson(json['ach_credit_transfer']) : null,
       amount: json['amount'],
-      client_secret: json['client_secret'],
+      clientSecret: json['client_secret'],
       created: json['created'],
       currency: json['currency'],
       flow: json['flow'],
@@ -51,7 +51,7 @@ class Source {
       livemode: json['livemode'],
       metadata: json['metadata'],
       owner: json['owner'] != null ? Owner.fromJson(json['owner']) : null,
-      statement_descriptor: json['statement_descriptor'],
+      statementDescriptor: json['statement_descriptor'],
       status: json['status'],
       type: json['type'],
       usage: json['usage'],
@@ -61,21 +61,21 @@ class Source {
   Map<String, dynamic> toJson() {
     final Map<String, dynamic> data = new Map<String, dynamic>();
     if (this.object != null) data['object'] = this.object;
-    if (this.client_secret != null) data['client_secret'] = this.client_secret;
+    if (this.clientSecret != null) data['client_secret'] = this.clientSecret;
     if (this.created != null) data['created'] = this.created;
     if (this.currency != null) data['currency'] = this.currency;
     if (this.flow != null) data['flow'] = this.flow;
     if (this.sourceId != null) data['sourceId'] = this.sourceId;
     if (this.livemode != null) data['livemode'] = this.livemode;
-    if (this.statement_descriptor != null) data['statement_descriptor'] = this.statement_descriptor;
+    if (this.statementDescriptor != null) data['statement_descriptor'] = this.statementDescriptor;
     if (this.status != null) data['status'] = this.status;
     if (this.type != null) data['type'] = this.type;
     if (this.usage != null) data['usage'] = this.usage;
     if (this.receiver != null) {
       data['receiver'] = this.receiver.toJson();
     }
-    if (this.ach_credit_transfer != null) {
-      data['ach_credit_transfer'] = this.ach_credit_transfer.toJson();
+    if (this.achCreditTransfer != null) {
+      data['ach_credit_transfer'] = this.achCreditTransfer.toJson();
     }
     if (this.amount != null) data['amount'] = this.amount;
     if (this.metadata != null) {
@@ -90,39 +90,39 @@ class Source {
 
 class Receiver {
   String address;
-  int amount_charged;
-  int amount_received;
-  int amount_returned;
-  String refund_attributes_method;
-  String refund_attributes_status;
+  int amountCharged;
+  int amountReceived;
+  int amountReturned;
+  String refundAttributesMethod;
+  String refundAttributesStatus;
 
   Receiver(
       {this.address,
-      this.amount_charged,
-      this.amount_received,
-      this.amount_returned,
-      this.refund_attributes_method,
-      this.refund_attributes_status});
+      this.amountCharged,
+      this.amountReceived,
+      this.amountReturned,
+      this.refundAttributesMethod,
+      this.refundAttributesStatus});
 
   factory Receiver.fromJson(Map<dynamic, dynamic> json) {
     return Receiver(
       address: json['address'],
-      amount_charged: json['amount_charged'],
-      amount_received: json['amount_received'],
-      amount_returned: json['amount_returned'],
-      refund_attributes_method: json['refund_attributes_method'],
-      refund_attributes_status: json['refund_attributes_status'],
+      amountCharged: json['amount_charged'],
+      amountReceived: json['amount_received'],
+      amountReturned: json['amount_returned'],
+      refundAttributesMethod: json['refund_attributes_method'],
+      refundAttributesStatus: json['refund_attributes_status'],
     );
   }
 
   Map<String, dynamic> toJson() {
     final Map<String, dynamic> data = new Map<String, dynamic>();
     if (this.address != null) data['address'] = this.address;
-    if (this.amount_charged != null) data['amount_charged'] = this.amount_charged;
-    if (this.amount_received != null) data['amount_received'] = this.amount_received;
-    if (this.amount_returned != null) data['amount_returned'] = this.amount_returned;
-    if (this.refund_attributes_method != null) data['refund_attributes_method'] = this.refund_attributes_method;
-    if (this.refund_attributes_status != null) data['refund_attributes_status'] = this.refund_attributes_status;
+    if (this.amountCharged != null) data['amount_charged'] = this.amountCharged;
+    if (this.amountReceived != null) data['amount_received'] = this.amountReceived;
+    if (this.amountReturned != null) data['amount_returned'] = this.amountReturned;
+    if (this.refundAttributesMethod != null) data['refund_attributes_method'] = this.refundAttributesMethod;
+    if (this.refundAttributesStatus != null) data['refund_attributes_status'] = this.refundAttributesStatus;
     return data;
   }
 }
@@ -132,20 +132,20 @@ class Owner {
   String email;
   String name;
   String phone;
-  BillingAddress verified_address;
-  String verified_email;
-  String verified_name;
-  String verified_phone;
+  BillingAddress verifiedAddress;
+  String verifiedEmail;
+  String verifiedName;
+  String verifiedPhone;
 
   Owner(
       {this.address,
       this.email,
       this.name,
       this.phone,
-      this.verified_address,
-      this.verified_email,
-      this.verified_name,
-      this.verified_phone});
+      this.verifiedAddress,
+      this.verifiedEmail,
+      this.verifiedName,
+      this.verifiedPhone});
 
   factory Owner.fromJson(Map<dynamic, dynamic> json) {
     return Owner(
@@ -153,10 +153,10 @@ class Owner {
       email: json['email'],
       name: json['name'],
       phone: json['phone'],
-      verified_address: json['verified_address'] != null ? BillingAddress.fromJson(json['verified_address']) : null,
-      verified_email: json['verified_email'],
-      verified_name: json['verified_name'],
-      verified_phone: json['verified_phone'],
+      verifiedAddress: json['verified_address'] != null ? BillingAddress.fromJson(json['verified_address']) : null,
+      verifiedEmail: json['verified_email'],
+      verifiedName: json['verified_name'],
+      verifiedPhone: json['verified_phone'],
     );
   }
 
@@ -166,40 +166,40 @@ class Owner {
     if (this.email != null) data['email'] = this.email;
     if (this.name != null) data['name'] = this.name;
     if (this.phone != null) data['phone'] = this.phone;
-    if (this.verified_address != null) data['verified_address'] = this.verified_address;
-    if (this.verified_email != null) data['verified_email'] = this.verified_email;
-    if (this.verified_name != null) data['verified_name'] = this.verified_name;
-    if (this.verified_phone != null) data['verified_phone'] = this.verified_phone;
+    if (this.verifiedAddress != null) data['verified_address'] = this.verifiedAddress;
+    if (this.verifiedEmail != null) data['verified_email'] = this.verifiedEmail;
+    if (this.verifiedName != null) data['verified_name'] = this.verifiedName;
+    if (this.verifiedPhone != null) data['verified_phone'] = this.verifiedPhone;
     return data;
   }
 }
 
 class AchCreditTransfer {
-  String account_number;
-  String bank_name;
+  String accountNumber;
+  String bankName;
   String fingerprint;
-  String routing_number;
-  String swift_code;
+  String routingNumber;
+  String swiftCode;
 
-  AchCreditTransfer({this.account_number, this.bank_name, this.fingerprint, this.routing_number, this.swift_code});
+  AchCreditTransfer({this.accountNumber, this.bankName, this.fingerprint, this.routingNumber, this.swiftCode});
 
   factory AchCreditTransfer.fromJson(Map<dynamic, dynamic> json) {
     return AchCreditTransfer(
-      account_number: json['account_number'],
-      bank_name: json['bank_name'],
+      accountNumber: json['account_number'],
+      bankName: json['bank_name'],
       fingerprint: json['fingerprint'],
-      routing_number: json['routing_number'],
-      swift_code: json['swift_code'],
+      routingNumber: json['routing_number'],
+      swiftCode: json['swift_code'],
     );
   }
 
   Map<String, dynamic> toJson() {
     final Map<String, dynamic> data = new Map<dynamic, dynamic>();
-    if (this.account_number != null) data['account_number'] = this.account_number;
-    if (this.bank_name != null) data['bank_name'] = this.bank_name;
+    if (this.accountNumber != null) data['account_number'] = this.accountNumber;
+    if (this.bankName != null) data['bank_name'] = this.bankName;
     if (this.fingerprint != null) data['fingerprint'] = this.fingerprint;
-    if (this.routing_number != null) data['routing_number'] = this.routing_number;
-    if (this.swift_code != null) data['swift_code'] = this.swift_code;
+    if (this.routingNumber != null) data['routing_number'] = this.routingNumber;
+    if (this.swiftCode != null) data['swift_code'] = this.swiftCode;
     return data;
   }
 }
