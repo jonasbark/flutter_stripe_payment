49f82f882ef0be6016d732a5c9c5c6deab0fcf7b
Alexey Yushin <ayushin@apexlabs.ai>
2020-05-02 10:08:49 +0200

    initial web setup

---
+++ example/lib/generated_plugin_registrant.dart
@@ -0,0 +1,16 @@
+//
+// Generated file. Do not edit.
+//
+
+// ignore: unused_import
+import 'dart:ui';
+
+import 'package:stripe_payment/src/web/flutter_stripe_payment_web.dart';
+
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
+
+// ignore: public_member_api_docs
+void registerPlugins(PluginRegistry registry) {
+  StripePaymentPlugin.registerWith(registry.registrarFor(StripePaymentPlugin));
+  registry.registerMessageHandler();
+}
