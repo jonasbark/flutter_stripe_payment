ade01c65de866cc788bd1ca0944c8ad7c1f2b548
tokenn <894266648@qq.com>
2022-05-11 15:05:24 +0800

    update com.stripe:stripe-android:16.10.0 to 16.10.2

--- example/android/app/src/main/kotlin/jonasbark/de/stripepaymentexample/MainActivity.kt
+++ example/android/app/src/main/kotlin/jonasbark/de/stripepaymentexample/MainActivity.kt
@@ -1,16 +1,10 @@
 package jonasbark.de.stripepaymentexample
 
 import android.os.Bundle
-import io.flutter.app.FlutterActivity
+import io.flutter.embedding.android.FlutterActivity;
 import io.flutter.plugins.GeneratedPluginRegistrant
 
 class MainActivity : FlutterActivity() {
 
-
-    @Override
-    protected override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        GeneratedPluginRegistrant.registerWith(this)
-    }
 }
 
